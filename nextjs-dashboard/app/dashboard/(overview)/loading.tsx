ebbd11c3acf7d86531e41be1939958d09ff0ca25
outlastzedd <admgnusam123@gmail.com>
2024-12-23 16:59:09 +0700

    half chap 11

---
+++ nextjs-dashboard/app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+// loading.tsx is used to display a loading UI while data is being fetched for the real UI to appear
+
+import DashboardSkeleton from "@/app/ui/skeletons"
+export default function Loading() {
+    return <DashboardSkeleton/>
+}
